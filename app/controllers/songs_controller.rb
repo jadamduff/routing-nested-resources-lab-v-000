14c6a40a8364ceb6d3f39a51fc8c176d2ee8a196
Adam Duff <j.adam.duff@gmail.com>
2018-10-01 16:05:19 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,7 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
-      if Artist.find(params[:artist_id])
+      if Artist.find(params[:artist_id]) != nil
         @songs = Artist.find(params[:artist_id]).songs
       else
         redirect_to artists_path
